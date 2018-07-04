a58335c19c35402ac4b91693994497dd7323a481
Alexander Gor <ornot.work@gmail.com>
2018-07-04 17:01:46 +0300

    T3-271. Migrations command, backup fab task

--- fabfile.py
+++ fabfile.py
@@ -5,13 +5,30 @@ from fabric.operations import local
 @task
 def build():
     with lcd('./deployment/'):
-        local('docker-compose build')
-        local('docker-compose up -d')
+        local('docker-compose up -d --build')
+
 
 @task
 def test():
     with lcd('./deployment/'):
         local('docker-compose exec authorization pytest')
+
+
+@task
+def backup(target='authorization'):
+    exclude = '--exclude auth.permission --exclude contenttypes'
+    local("docker exec {} mkdir -p /home/backup".format(target))
+    local("docker exec {} sh -c 'python manage.py dumpdata {} > /home/backup/latest.json'".format(target, exclude))
+
+
+@task
+def migrate(target='authorization'):
+    local("docker exec {} python manage.py migrate".format(target))
+
+@task
+def restore(target):
+    pass
+
 
 @task
 def cleanup():
