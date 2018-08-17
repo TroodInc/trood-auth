edbea9911864d0ccf4522082b384dd65fb14737d
Sergey Dubinin <whackojacko.ru@gmail.com>
2018-08-17 12:06:30 +0500

    TB-156: Dockerfile update: specified exact python version

--- t_auth/settings.py
+++ t_auth/settings.py
@@ -152,7 +152,7 @@ class BaseConfiguration(Configuration):
 
 
 try:
-    from custom_configuration import CustomConfiguration
+    from .custom_configuration import CustomConfiguration
 except ImportError:
     class CustomConfiguration:
         pass
